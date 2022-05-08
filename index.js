185430f0b23bcf90685c2ade68e80a77ead2069d
ashishmohapatra240 <ashishmohapatra240@gmail.com>
2022-05-08 20:07:45 +0530

    Ready

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ import express from "express";
 import bodyParser from 'body-parser';
 import usersRoutes from './routes/user.js';
 
-const app=express();
-const PORT=5000;
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
 app.use('/users', usersRoutes)
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('Hello There');
 });
 
-app.listen(PORT,()=>console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
